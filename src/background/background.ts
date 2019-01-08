694ce64212bdabdbbb69d91e6efb7ab076fc08c3
dzianis_viarshadau <day85@tut.by>
2019-01-08 12:40:07 +0300

    Separated extension installation and initialisation.

--- src/background/background.ts
+++ src/background/background.ts
@@ -13,6 +13,8 @@ class Background {
     public enableExtension(tabId: number, tab: any): void {
         if (this.isEligible(tab.url)) {
             background.browser.enable(tabId);
+        } else {
+            background.browser.disable();
         }
     }
 
@@ -26,72 +28,72 @@ class Background {
 const background = new Background(BrowserApiWrapper.instance);
 
 background.browser.onInstalled(() => {
+    console.log('Extension was successfully installed.');
     background.browser.disable();
+});
 
-    background.browser.tabsOnActivated((activeInfo: any) => {
-        background.browser.tabsGet(activeInfo.tabId)
-            .then((tab: number) => {
-                background.enableExtension(activeInfo.tabId, tab);
-            }).catch(() => console.log('Can not enable extension on current tab.'));
-    });
+background.browser.tabsOnActivated((activeInfo: any) => {
+    background.browser.tabsGet(activeInfo.tabId)
+        .then((tab: number) => {
+            background.enableExtension(activeInfo.tabId, tab);
+        }).catch(() => console.log('Can not enable extension on current tab.'));
+});
 
-    background.browser.tabsOnUpdated((tabId: number, changeInfo: any, tab: any) => {
-        background.enableExtension(tabId, tab);
-    });
+background.browser.tabsOnUpdated((tabId: number, changeInfo: any, tab: any) => {
+    background.enableExtension(tabId, tab);
+});
 
-    window.setInterval(() => {
-        background.browser.tabsQuery({currentWindow: true, active: true})
-            .then((tabs: any) => {
-                if (tabs.length && tabs[0].id) {
-                    background.browser.tabsSendMessage(tabs[0].id,
-                        {background: {message: 'Hello from background!'}})
-                        .then((response: any) => {
-                            if (response && response.content) console.log(response.content.response);
-                        }).catch(() => console.log('The current tab is not allowed in permissions.'));
-                }
-            });
-    }, 5000);
-
-
-    background.browser.receiveMessage((receive: any, sender: any, sendResponse: any) => {
-        if (receive && receive.content) console.log(receive.content.message);
-        if (receive && receive.popup) console.log(receive.popup.message);
-        if (receive) sendResponse({background: {response: 'Response from Background!'}});
-    });
-
-
-    let cssFlag = false;
-    let jsFlag = false;
-
-    background.browser.receiveCommand((command: string) => {
-        console.log('Command:', command);
-        if (command === 'injection') {
-            background.browser.executeScript(undefined, {file: 'scripts/injection.js'});
-            background.browser.insertCSS(undefined, {file: 'styles/injection.css'});
-        }
-
-        if (command === 'change-css') {
-            if (cssFlag) {
-                background.browser.insertCSS(undefined, {code: 'body{border:solid 4px green}'});
-                cssFlag = false;
-            } else {
-                background.browser.insertCSS(undefined, {code: 'body{border:solid 4px red}'});
-                cssFlag = true;
+window.setInterval(() => {
+    background.browser.tabsQuery({currentWindow: true, active: true})
+        .then((tabs: any) => {
+            if (tabs.length && tabs[0].id) {
+                background.browser.tabsSendMessage(tabs[0].id,
+                    {background: {message: 'Hello from background!'}})
+                    .then((response: any) => {
+                        if (response && response.content) console.log(response.content.response);
+                    }).catch(() => console.log('The current tab is not allowed in permissions.'));
             }
-        }
+        });
+}, 5000);
 
-        if (command === 'change-js') {
-            if (jsFlag) {
-                background.browser.executeScript(undefined,
-                    {code: 'document.querySelectorAll("p").forEach(p=>p.style.color="gray")'});
-                jsFlag = false;
-            } else {
-                background.browser.executeScript(undefined,
-                    {code: 'document.querySelectorAll("p").forEach(p=>p.style.color="red")'});
-                jsFlag = true;
-            }
-        }
-    });
 
+background.browser.receiveMessage((receive: any, sender: any, sendResponse: any) => {
+    if (receive && receive.content) console.log(receive.content.message);
+    if (receive && receive.popup) console.log(receive.popup.message);
+    if (receive) sendResponse({background: {response: 'Response from Background!'}});
+});
+
+
+let cssFlag = false;
+let jsFlag = false;
+
+background.browser.receiveCommand((command: string) => {
+    console.log('Command:', command);
+    if (command === 'injection') {
+        background.browser.executeScript(undefined, {file: 'scripts/injection.js'});
+        background.browser.insertCSS(undefined, {file: 'styles/injection.css'});
+    }
+
+    if (command === 'change-css') {
+        if (cssFlag) {
+            background.browser.insertCSS(undefined, {code: 'body{border:solid 4px green}'});
+            cssFlag = false;
+        } else {
+            background.browser.insertCSS(undefined, {code: 'body{border:solid 4px red}'});
+            cssFlag = true;
+        }
+    }
+
+    if (command === 'change-js') {
+        if (jsFlag) {
+            background.browser.executeScript(undefined,
+                {code: 'document.querySelectorAll("p").forEach(p=>p.style.color="gray")'});
+            jsFlag = false;
+        } else {
+            background.browser.executeScript(undefined,
+                {code: 'document.querySelectorAll("p").forEach(p=>p.style.color="red")'});
+            jsFlag = true;
+        }
+    }
 });
 
